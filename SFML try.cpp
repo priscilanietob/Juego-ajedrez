58590efcbd375435d1f95e32a23ca6c775e985f1
alanloom <165715544+alanloom@users.noreply.github.com>
2024-05-12 21:32:11 -0700

    Update SFML try.cpp

--- SFML try.cpp
+++ SFML try.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using namespace sf;
 
-// FunciÛn para cargar texturas desde un archivo
+// Funci√≥n para cargar texturas desde un archivo
 bool cargarTextura(Texture& textura, const string& archivo)
 {
     if (!textura.loadFromFile(archivo))
@@ -16,6 +16,13 @@ bool cargarTextura(Texture& textura, const string& archivo)
 
 int main()
 {
+    //carga la tipografia arial para las coordenadas
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf.ttf")) { //busca el archivo de font y despliega error si no se encuentra.
+        std::cerr << "Error al cargar el archivo de letra." << std::endl;
+        return 0;
+    }
+    
     RenderWindow ventana(VideoMode(600, 330), "Main Menu"); //Crear ventana para el menu de inicio
 
     Texture texturaFondo, texturaPlay, texturaSettings, texturaExit; //Crear las texturas de los botones
@@ -25,7 +32,7 @@ int main()
     cargarTextura(texturaExit, "Exit_resized.png");
 
     Sprite spriteFondo(texturaFondo);//Creo el sprite del fondo
-    spriteFondo.setScale((float)ventana.getSize().x / spriteFondo.getTexture()->getSize().x, (float)ventana.getSize().y / spriteFondo.getTexture()->getSize().y); //Ajusta el tamaÒo del sprite del fondo a la ventana
+    spriteFondo.setScale((float)ventana.getSize().x / spriteFondo.getTexture()->getSize().x, (float)ventana.getSize().y / spriteFondo.getTexture()->getSize().y); //Ajusta el tama√±o del sprite del fondo a la ventana
 
     Sprite play(texturaPlay), Settings(texturaSettings), Exit(texturaExit); //Posicion de las texturas de botones (debe ser la misma que de los botones)
     play.setPosition(100, 50);
@@ -33,7 +40,7 @@ int main()
     Exit.setPosition(100, 250);
 
     // Creacion de botones
-    RectangleShape botonPlay(Vector2f(200, 60)); //TamaÒo y posicion de los botones
+    RectangleShape botonPlay(Vector2f(200, 60)); //Tama√±o y posicion de los botones
     botonPlay.setPosition(100, 50); 
 
     RectangleShape botonSettings(Vector2f(200, 60));
@@ -72,31 +79,31 @@ int main()
                         cargarTextura(texturaPeonNegro, "peon_negro_resized.png");              
                         
                         //TEXTURAS DE TORRES/ROOKS
-                        Texture texturaBRook, texturaWRook;
-                        cargarTextura(texturaBRook, "Rook B_resized.png");
-                        cargarTextura(texturaWRook, "Rook W_resized.png");   
+                        Texture texturaTorre_Negra, texturaTorre_Blanca;
+                        cargarTextura(texturaTorre_Negra, "Torre_Negra.png");
+                        cargarTextura(texturaTorre_Blanca, "Torre_Blanca.png");   
                         
                         //TEXTURAS DE CABALLOS/KNIGHTS
-                        Texture texturaBKnight, texturaWKnight;
-                        cargarTextura(texturaBKnight, "BKnight_resized.png");
-                        cargarTextura(texturaWKnight, "WKnight_resized.png");
+                        Texture texturaCaballo_Negro, texturaCaballo_Blanco;
+                        cargarTextura(texturaCaballo_Negro, "Caballo_Negro.png");
+                        cargarTextura(texturaCaballo_Blanco, "Caballo_Blanco.png");
 
                         //TEXTURAS DE ALFIL/BISHOP
-                        Texture texturaBBishop, texturaWBishop;
-                        cargarTextura(texturaBBishop, "BBishop_resized.png");
-                        cargarTextura(texturaWBishop, "WBishop_resized.png");
+                        Texture texturaAlfil_Negro, texturaAlfil_Blanco;
+                        cargarTextura(texturaAlfil_Negro, "Alfil_Negro.png");
+                        cargarTextura(texturaAlfil_Blanco, "Alfil_Blanco.png");
 
                         //TEXCTURAS DE LA REINA/QUEEN
-                        Texture texturaBQueen, texturaWQueen;
-                        cargarTextura(texturaBQueen, "BQueen_resized.png");
-                        cargarTextura(texturaWQueen, "WQueen_resized.png");
+                        Texture texturaReina_Negra, texturaReina_Blanca;
+                        cargarTextura(texturaReina_Negra, "Reina_Negra.png");
+                        cargarTextura(texturaReina_Blanca, "Reina_Blanca.png");
 
                         //TEXTURAS DEL REY/KING
-                        Texture texturaBKing, texturaWKing;
-                        cargarTextura(texturaBKing, "BKing_resized.png");
-                        cargarTextura(texturaWKing, "WKing_resized.png");
+                        Texture texturaRey_Negro, texturaRey_Blanco;
+                        cargarTextura(texturaRey_Negro, "Rey_Negro.png");
+                        cargarTextura(texturaRey_Blanco, "Rey_Blanco.png");
 
-                        Sprite BRook, WRook, BKnight, WKnight, WBishop, BBishop, BQueen, WQueen, WKing, BKing;
+                        Sprite Torre_Negra, Torre_Blanca, Caballo_Negro, Caballo_Blanco, Alfil_Blanco, Alfil_Negro, Reina_Negra, Reina_Blanca, Rey_Blanco, Rey_Negro;
 
                         // Posiciones iniciales de los peones
                         const int numPeones = 8;
@@ -134,7 +141,31 @@ int main()
                                 colorBlanco = !colorBlanco;
                             }
 
+                                //define el texto, su tamanio y color
+                                sf::Text text;
+                                text.setFont(font);
+                                text.setCharacterSize(30);
+                                text.setFillColor(sf::Color::White);
 
+                                // posiciones iniciales en la ventana para hacer la impresion de letras y numeros
+                                int letrasss = 30;
+                                int numeros = 40;
+
+                                // Dibujar las letras (A-H)
+                                for (int i = 0; i < 8; ++i) {
+                                    char letras = 'A' + i;
+                                    text.setString(std::string(1, letras));
+                                    text.setPosition(letrasss + i * 100, 800);
+                                    ventana.draw(text);
+                                }
+
+                                // Dibujar los n√∫meros (1-8)
+                                for (int i = 0; i < 8; ++i) {
+                                    char num = '8' - i;
+                                    text.setString(std::string(1, num));
+                                    text.setPosition(815, numeros + i * 100);
+                                    ventana.draw(text);
+                                }
                             // Dibujar los peones blancos
                             for (int i = 0; i < numPeones; i++)
                             {
@@ -149,53 +180,53 @@ int main()
                             //Dibujar los rooks, caballos y bishops
                             for (int i = 0; i < 2; i++) 
                             {
-                                WRook.setTexture(texturaWRook);
-                                BRook.setTexture(texturaBRook);    
+                                Torre_Blanca.setTexture(texturaTorre_Blanca);
+                                Torre_Negra.setTexture(texturaTorre_Negra);    
 
-                                WKnight.setTexture(texturaWKnight);
-                                BKnight.setTexture(texturaBKnight);
+                                Caballo_Blanco.setTexture(texturaCaballo_Blanco);
+                                CaballoNegro.setTexture(texturaCaballoNegro);
 
-                                WBishop.setTexture(texturaWBishop);
-                                BBishop.setTexture(texturaBBishop);
+                                Alfil_Blanco.setTexture(texturaAlfil_Blanco);
+                                Alfil_Negro.setTexture(texturaAlfil_Negro);
 
-                                BRook.setPosition(i * 700, 0);
-                                WRook.setPosition(i * 700, 700);
+                                Torre_Negra.setPosition(i * 700, 0);
+                                Torre_Blanca.setPosition(i * 700, 700);
                                 
-                                BKnight.setPosition(i * 500 + 100, 0);
-                                WKnight.setPosition(i * 500 + 100, 700);
+                                Caballo_Negro.setPosition(i * 500 + 100, 0);
+                                Caballo_Blanco.setPosition(i * 500 + 100, 700);
 
-                                BBishop.setPosition(i * 300 + 200, 0);
-                                WBishop.setPosition(i * 300 + 200, 700);
+                                Alfil_Negro.setPosition(i * 300 + 200, 0);
+                                Alfil_Blanco.setPosition(i * 300 + 200, 700);
 
-                                ventana.draw(WRook);
-                                ventana.draw(BRook);
+                                ventana.draw(Torre_Blanca);
+                                ventana.draw(Torre_Negra);
                                 
-                                ventana.draw(WKnight);
-                                ventana.draw(BKnight);
+                                ventana.draw(Caballo_Blanco);
+                                ventana.draw(Caballo_Negro);
 
-                                ventana.draw(WBishop);
-                                ventana.draw(BBishop);
+                                ventana.draw(Alfil_Blanco);
+                                ventana.draw(Alfil_Negro);
                             }       
 
                             //QUEEN
-                            WQueen.setTexture(texturaWQueen);
-                            BQueen.setTexture(texturaBQueen);
+                            Reina_Blanca.setTexture(texturaReina_Blanca);
+                            Reina_Negra.setTexture(texturaReina_Negra);
 
-                            BQueen.setPosition(300, 0);
-                            WQueen.setPosition(300, 700);
+                            Reina_Negra.setPosition(300, 0);
+                            Reina_Blanca.setPosition(300, 700);
 
-                            ventana.draw(WQueen);
-                            ventana.draw(BQueen);
+                            ventana.draw(Reina_Blanca);
+                            ventana.draw(Reina_Negra);
 
                             //KING
-                            WKing.setTexture(texturaWKing);
-                            BKing.setTexture(texturaBKing);
+                            Rey_Blanco.setTexture(texturaRey_Blanco);
+                            Rey_Negro.setTexture(texturaRey_Negro);
 
-                            BKing.setPosition(400, 0);
-                            WKing.setPosition(400, 700);
+                            Rey_Negro.setPosition(400, 0);
+                            Rey_Blanco.setPosition(400, 700);
 
-                            ventana.draw(WKing);
-                            ventana.draw(BKing);
+                            ventana.draw(Rey_Blanco);
+                            ventana.draw(Rey_Negro);
 
                             ventana.draw(botonExitGame);
                             
@@ -203,7 +234,7 @@ int main()
                             ventana.display();
 
                         }
-                        cout << "°Jugar ajedrez!" << endl;
+                        cout << "¬°Jugar ajedrez!" << endl;
 
                     }
                     else if (botonExit.getGlobalBounds().contains(mousePos.x, mousePos.y))
